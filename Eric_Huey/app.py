2baa21b9f651dd65f0e3ae02ed8f41dd59557efd
Eric Huey <erichuey@Erics-MacBook-Pro.local>
2020-02-03 20:33:33 -0800

    app.py completed

--- Eric_Huey/app.py
+++ Eric_Huey/app.py
@@ -5,28 +5,65 @@ from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
 
-app.config["SECRET_KEY"] = ""
+app.config["SECRET_KEY"] = "fInAlPrOjEcTs"
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+    return render_template("about_us.html")
+
 @app.route("/deep", methods=["GET", "POST"])
 def deep_learning():
     if request.method == "POST":
+        input_age = request.form["name"]
+        gender = request.form["gender"]
+        height = request.form["height"]
+        weight = request.form["weight"]
+        cholesterol = request.form["chol"]
+        bp_lo = request.form["bp_lo"]
+        bp_hi = request.form["bp_hi"]
+        glucose = request.form["glucose"]
+        smoke = request.form["smoke"]
+        alc = request.form["alc"]
+        active = request.form["active"]
 
-    return render_template("deep.html")
+    return render_template("deep_learning.html")
 
-@app.route("/forest", methods=["GET", "POST"])
+@app.route("/svm", methods=["GET", "POST"])
 def random_forest():
     if request.method == "POST":
+        input_age = request.form["name"]
+        gender = request.form["gender"]
+        height = request.form["height"]
+        weight = request.form["weight"]
+        cholesterol = request.form["chol"]
+        bp_lo = request.form["bp_lo"]
+        bp_hi = request.form["bp_hi"]
+        glucose = request.form["glucose"]
+        smoke = request.form["smoke"]
+        alc = request.form["alc"]
+        active = request.form["active"]
+        
+    return render_template("svm_gs.html")
 
-    return render_template("forest.html")
-
-@app.route("logreg/", methods=["GET", "POST"])
+@app.route("/logreg", methods=["GET", "POST"])
 def log_regression():
     if request.method == "POST":
-        
+        input_age = request.form["name"]
+        gender = request.form["gender"]
+        height = request.form["height"]
+        weight = request.form["weight"]
+        cholesterol = request.form["chol"]
+        bp_lo = request.form["bp_lo"]
+        bp_hi = request.form["bp_hi"]
+        glucose = request.form["glucose"]
+        smoke = request.form["smoke"]
+        alc = request.form["alc"]
+        active = request.form["active"]
+
     return render_template("logreg.html")
 
 if __name__ == "__main__":
