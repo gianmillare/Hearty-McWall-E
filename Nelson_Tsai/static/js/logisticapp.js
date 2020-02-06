a2f3f0133ecacac67ba3c9f3387a8ed3c5b2f410
nelsont87 <nelsont87@gmail.com>
2020-02-06 12:22:03 -0800

    modified some text

--- Nelson_Tsai/static/js/logisticapp.js
+++ Nelson_Tsai/static/js/logisticapp.js
@@ -6,50 +6,43 @@ function animation() {
         };
 
         var canvasR = document.getElementById("result");
-        var ctxR = canvasR.getContext("2d");
-        ctxR.font = "30px Georgia";
-        ctxR.fillStyle = "blue";
-        ctxR.textAlign = "center";
         if (data["prediction"][0][0]>data["prediction"][0][1]){
-            ctxR.fillText(data["prediction"][0][0] + " probability of being healthy!", canvasR.width/2, canvasR.height/2);
+            canvasR.textContent += parseFloat(data["prediction"][0][0]).toFixed(2)+"%" + " probability of being healthy!"
             gsap.to("#healthy", {autoAlpha:1, visibility: "visible", duration:1});
         } else {
-            ctxR.fillText(data["prediction"][0][1] + " probability of having cardiovascular disease. Consult your doctor.", canvasR.width/2, canvasR.height/2);
+            canvasR.textContent += parseFloat(data["prediction"][0][1]).toFixed(2)+"%" + "probability of cardiovascular issues. Consult your Doctor."
             gsap.to("#doctor", {autoAlpha:1, visibility: "visible", duration:1});
         };
-        console.log(fix);
+
         if (fix != 0){
-            var canvas = document.getElementById("fix");
-            var ctx = canvas.getContext("2d");
-            ctx.font = "30px Georgia";
-            ctx.fillStyle = "blue";
-            ctx.textAlign = "center";
+            var ctx = document.getElementById("fix");
+
             if (fix == "cholesterol"){
-                ctx.fillText("Be careful of cholesterol intake.", canvas.width/2, canvas.height/2);
+                ctx.textContent += "Be careful of cholesterol intake.";
                 gsap.to("#cholC", {autoAlpha:1, visibility: "visible", duration:1});
             } 
             else if (fix == "weight"){
-                ctx.fillText("Motivate yourself to shed some unnecessary weight.", canvas.width/2, canvas.height/2);
+                ctx.textContent += "Motivate yourself to shed some unnecessary weight.";
                 gsap.to("#weightC", {autoAlpha:1, visibility: "visible", duration:1});
             }
             else if (fix == "alcohol"){
-                ctx.fillText("Lessen your intake of alcohol.", canvas.width/2, canvas.height/2);
+                ctx.textContent += "Lessen your intake of alcohol.";
                 gsap.to("#alcoC", {autoAlpha:1, visibility: "visible", duration:1});
             }
             else if (fix == "BP"){
-                ctx.fillText("Monitor your blood pressure closely.", canvas.width/2, canvas.height/2);
+                ctx.textContent += "Monitor your blood pressure closely.";
                 gsap.to("#BP", {autoAlpha:1, visibility: "visible", duration:1});
             }
             else if (fix = "smoking"){
-                ctx.fillText("Time to help others and yourself by not smoking.", canvas.width/2, canvas.height/2);
+                ctx.textContent += "Time to help others and yourself by not smoking.";
                 gsap.to("#smokeC", {autoAlpha:1, visibility: "visible", duration:1});
             }
             else if (fix == "glucose"){
-                ctx.fillText("Be careful of sugar intake.", canvas.width/2, canvas.height/2);
+                ctx.textContent += "Be careful of sugar intake.";
                 gsap.to("#glucC", {autoAlpha:1, visibility: "visible", duration:1});
             }
             else if (fix == "active"){
-                ctx.fillText("Motivate yourself to move.", canvas.width/2, canvas.height/2);
+                ctx.textContent += "Motivate yourself to move.";
                 gsap.to("#activeC", {autoAlpha:1, visibility: "visible", duration:1});
             }
         };
