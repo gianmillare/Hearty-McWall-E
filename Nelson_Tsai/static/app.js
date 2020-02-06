de38dc75d95dbef2fa1fc970292f9b25463c774f
nelsont87 <nelsont87@gmail.com>
2020-02-06 09:42:37 -0800

    app.py updated

--- Nelson_Tsai/static/app.js
+++ Nelson_Tsai/static/app.js
@@ -1,4 +1,9 @@
-
-d3.json('/deepstat').then(function(data) {
-    // grab a div from deep_learning.html to input in a gif
-})
+function animation() {
+    d3.json(`/deepstat`).then(function(data) {
+        fix = 0
+        res = 0
+        if (data["improvement"] != 0 ){
+            res = data["improvement"]
+        }
+    });
+};
